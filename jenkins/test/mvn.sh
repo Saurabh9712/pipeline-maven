9640528dd452b46f09c29540e00dc7ad68a3653e
saurabh <saurabh@gmail.com>
2025-04-01 08:42:42 +0530

    fixes workspace apth for jobs eexecution

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -6,4 +6,6 @@ echo "***************************"
 
 #WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
 
-docker run --rm  -v $PWD/java-app:/app -v $HOME/root/.m2:/root/.m2 -w /app maven:3.9.9 "$@"
+WORKSPACE=/var/jenkins_home/workspace/pipeline-maven
+
+docker run --rm  -v $WORKSPACE/java-app:/app -v $HOME/root/.m2:/root/.m2 -w /app maven:3.9.9 "$@"
